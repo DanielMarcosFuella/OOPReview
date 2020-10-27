32dc8995c077c741e19e50d172205cd1b9d7a23f
DanielMarcosFuella <Daniel.Marcos.Fuella@hotmail.com>
2020-10-27 15:26:06 +0100

    subida definitiva

--- Repaso2.2.ts
+++ Repaso2.2.ts
@@ -4,6 +4,14 @@ let movil1:Mobile = new Mobile("Nokia3210", 3210, "Nokia", 320, "Perla", false, 
 let movil2:Mobile = new Mobile("iPhone3G", 3, "iPhone", 500, "Oro", false, 3, 900)
 let movil3:Mobile = new Mobile("Samsug Galaxy 10", 10, "Samgug", 640, "Blanco", true, 4, 1200)
 
-movil1.is5G=true
-movil1.cameraNumber=4
-console.log(movil1,movil2,movil3)
+// movil1.setis5G(true)
+// movil1.setCameraNumber(4)
+// console.log(movil1.toStrong())
+// console.log(movil2.toStrong())
+// console.log(movil3.toStrong())
+let moviles:Mobile[]
+moviles=[movil1,movil2,movil3]
+for (let i=0;i<moviles.length;i++){
+    console.log(moviles[i].toStrong())
+}
+
--- Repaso2.ts
+++ Repaso2.ts
@@ -1,12 +1,12 @@
 export class Mobile{
-    name:string;
-    model:number
-    trademark:string
-    sdSize:number
-    color:string
-    is5G:boolean
-    cameraNumber:number
-    price:number
+    private name:string;
+    private model:number
+    private trademark:string
+    private sdSize:number
+    private color:string
+    private is5G:boolean
+    private cameraNumber:number
+    private price:number
     constructor(name:string, model:number,trademark:string, sdSize:number, color:string,
          is5G:boolean,cameraNumber:number,price:number){
             this.name=name
@@ -18,5 +18,64 @@ export class Mobile{
             this.cameraNumber=cameraNumber
             this.price=price
     }
+    public GetName():string{
+        return this.name
+    }
+    public GetModel():number{
+        return this.model
+    }
+    public GetTrademark():string{
+        return this.trademark
+    }
+    public GetsdSize():number{
+        return this.sdSize
+    }
+    public GetColor():string{
+        return this.color
+    }
+    public Getis5G():boolean{
+        return this.is5G
+    }
+    public GetCameraNumber():number{
+        return this.cameraNumber
+    }
+    public GetPrice():number{
+        return this.price
+    }
+    public setName(newName:string){
+        this.name=newName
+    }
+    public setModel(newModel:number){
+        this.model=newModel
+    }
+    public setTrademark(newTrademark:string){
+        this.trademark=newTrademark
+    }
+    public setsdSize(newsdSize:number){
+        this.sdSize=newsdSize
+    }
+    public setColor(newColor:string){
+        this.color=newColor
+    }
+    public setis5G(newis5G:boolean){
+        this.is5G=newis5G
+    }
+    public setCameraNumber(newCameraNumber:number){
+        this.cameraNumber=newCameraNumber
+    }
+    public setPrice(newPrice:number){
+        this.price=newPrice
+    }
+    public toStrong():string{
+        return "The characteristics of the mobile " + this.name +" are:" + "\n" +
+        "Name: " +this.name + "\n" +
+        "Model: " +this.model + "\n" +
+        "Trademark: " +this.trademark + "\n" +
+        "SD Size (GB): " + this.sdSize + "\n" +
+        "Color: " +this.sdSize + "\n" +
+        "Is 5g?: " +this.is5G + "\n" +
+        "Number of Cameras: " +this.cameraNumber 
+    }
 }
+
 
